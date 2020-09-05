3516a4037c60590c23e5683c9afa14ceda2971d9
gziltoid <g.ziltoid@gmail.com>
2020-09-05 16:14:59 -0700

    initial commit

---
+++ weather_bot.py
@@ -0,0 +1,79 @@
+import os
+import telebot
+from dotenv import load_dotenv, find_dotenv
+import requests
+
+load_dotenv(find_dotenv())
+TOKEN = os.getenv('BOT_TOKEN', 'BOT_TOKEN does not exist.')
+
+UNITS = 'metric'
+LOCATION = 'Moscow'
+LANGUAGE = 'en'
+GET_NOTIFICATIONS = False
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(
+        message.chat.id,
+        '''Hey! I'm the Weather Cat.
+I can show you the weather forecast up for 5 days.
+Just send me one of these commands:
+/current - get the current weather for a location or city
+/weatherfull - get a 5-day forecast for a location or city
+/changelocation - set or change your location
+/lang - select a language
+/units - change your unit preferences
+/notifications - enable notifications
+'''
+)
+
+@bot.message_handler(commands=['current'])
+def get_current_weather(message):
+    bot.send_message(message.chat.id, 'Current weather')
+
+@bot.message_handler(commands=['weatherfull'])
+def get_forecast(message):
+    bot.send_message(message.chat.id, '5-day forecast')
+
+@bot.message_handler(commands=['changelocation'])
+def change_location(message):
+    bot.send_message(
+        message.chat.id,
+        f'Current location: {LOCATION}.\nSend location pin or enter your area or city:')
+
+@bot.message_handler(commands=['lang'])
+def change_language(message):
+    bot.send_message(message.chat.id, f'Current language: {LANGUAGE}.\nSelect a language:')
+
+@bot.message_handler(commands=['units'])
+def change_units(message):
+    bot.send_message(
+        message.chat.id,
+        f'Current units: {UNITS}.\nIf you want to change it, please type metric or imperial:')
+
+@bot.message_handler(commands=['notifications'])
+def set_notifications(message):
+    bot.send_message(
+        message.chat.id,
+        f'Enabled: {"Yes" if GET_NOTIFICATIONS else "No"}.\nDo you want to get daily notifications? Type Yes or No.')
+
+@bot.message_handler(func=lambda message: True)
+def weather(message):
+    global UNITS
+    if str.lower(message.text) == 'metric':
+        UNITS = 'metric'
+        bot.send_message(message.chat.id, f'Done. Updated units to {UNITS}.')
+    elif str.lower(message.text) == 'imperial':
+        UNITS = 'imperial'
+        bot.send_message(message.chat.id, f'Done. Updated units to {UNITS}.')
+    else:
+        bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
+
+
+bot.polling()
