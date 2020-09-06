806520f29cf1925d147cfeaa8fa2ec20a7ddabb7
gziltoid <g.ziltoid@gmail.com>
2020-09-06 02:09:41 -0700

    cleanup

--- weather_bot.py
+++ weather_bot.py
@@ -6,19 +6,36 @@ from pprint import pprint
 
 load_dotenv(find_dotenv())
 
-UNITS = 'metric'
-LOCATION = 'Moscow'
-LANGUAGE = 'en'
-GET_NOTIFICATIONS = False
-
+# Environment variables
 TOKEN = os.getenv('BOT_TOKEN', 'BOT_TOKEN does not exist.')
+OWM_API_KEY = os.getenv('OWM_API_KEY', 'OWM_API_KEY does not exist.')
+
+# Constants
+UNITS_METRIC = 'metric'
+UNITS_IMPERIAL = 'imperial'
+UNITS = (UNITS_METRIC, UNITS_IMPERIAL)
+DEGREE_SIGNS = {UNITS_METRIC: '℃', UNITS_IMPERIAL: '℉'}
+
+LANGUAGE_ENGLISH = 'en'
+LANGUAGE_RUSSIAN = 'ru'
+LANGUAGES = [LANGUAGE_ENGLISH, LANGUAGE_RUSSIAN]
+
+LOCATION_DEFAULT = 'Moscow'
+
+# Globals
+
+units = UNITS_METRIC
+language = LANGUAGE_ENGLISH
+location = LOCATION_DEFAULT
+are_notifications_enabled = False
+
 bot = telebot.TeleBot(TOKEN)
 
 API_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 querystring = {
-    'q': LOCATION,
-    'units': UNITS,
-    'appid': os.getenv('API_KEY', 'API_KEY does not exist.')
+    'q': location,
+    'units': units,
+    'appid': OWM_API_KEY,
 }
 
 
@@ -27,15 +44,14 @@ def send_welcome(message):
     bot.send_message(
         message.chat.id,
         '''Hey, I'm the Weather Cat.
-I can get you the weather forecast up for 5 days.
+I can show you the weather forecast up to 5 days.
 Just send me one of these commands:
 /current - get the current weather for a location or city
-/weatherfull - get a 5-day forecast for a location or city
+/forecast - get a 5-day forecast for a location or city
 /settings - change your preferences
 
 To start, send a location pin or enter your area or city:
-'''
-    )
+''')
 
 
 @bot.message_handler(commands=['help'])
@@ -45,25 +61,26 @@ def send_help(message):
         '''What can I do?
 Just send me one of these commands:
 /current - get the current weather for a location or city
-/weatherfull - get a 5-day forecast for a location or city
+/forecast - get a 5-day forecast for a location or city
 /settings - change your preferences
 ''')
 
 
 @bot.message_handler(commands=['current'])
 def get_current_weather(message):
-    response = requests.request("GET", API_URL, params=querystring).json()
+    response = requests.get(API_URL, params=querystring).json()
     city = response['city']['name']
-    temp = response['list'][0]['main']['temp']
+    temp = int(round(response['list'][0]['main']['temp']))
     desc = response['list'][0]['weather'][0]['description']
     # pprint(response)
     print(f"Current weather in {city}: {temp}, {desc}")
-    bot.send_message(message.chat.id, f"Current weather in {city}: {temp}, {desc}")
+    bot.send_message(message.chat.id, f"Current weather in {city}: {temp}{DEGREE_SIGNS[units]}, {desc}")
 
 
-@bot.message_handler(commands=['weatherfull'])
+@bot.message_handler(commands=['forecast'])
 def get_forecast(message):
     bot.send_message(message.chat.id, '5-day forecast:')
+    # TODO
 
 
 @bot.message_handler(commands=['settings'])
@@ -71,59 +88,57 @@ def show_settings(message):
     bot.send_message(
         message.chat.id,
         '''Settings:
-/changelocation - set or change your location
+/location - set or change your location
 /lang - select a language
 /units - change your unit preferences
-/notifications - enable notifications'''
-    )
+/notifications - enable or disable notifications
+''')
 
 
-@bot.message_handler(commands=['changelocation'])
+@bot.message_handler(commands=['location'])
 def change_location(message):
     bot.send_message(
         message.chat.id,
-        f'Current location: {LOCATION}.\nSend location pin or enter your area or city:')
+        f'Current location: {location}.\nSend location pin or enter your area or city:')
+    # TODO
 
 
 @bot.message_handler(commands=['lang'])
 def change_language(message):
-    bot.send_message(message.chat.id, f'Current language: {LANGUAGE}.\nSelect a language:')
+    bot.send_message(message.chat.id, f'Current language: {language}.\nSelect a language:')
+    # TODO
 
 
 @bot.message_handler(commands=['units'])
 def change_units(message):
     bot.send_message(
         message.chat.id,
-        f'Current units: {UNITS}.\nIf you want to change it, please type metric or imperial:')
+        f'Current units: {units}.\nIf you want to change it, please type metric or imperial:')
+    # TODO
 
 
 @bot.message_handler(commands=['notifications'])
 def set_notifications(message):
     bot.send_message(
         message.chat.id,
-        f'Enabled: {"Yes" if GET_NOTIFICATIONS else "No"}.\nDo you want to get daily notifications? Type Yes or No.')
+        f'Enabled: {"Yes" if are_notifications_enabled else "No"}.\nDo you want to get daily notifications? Type Yes or No.')
+    # TODO
 
 
 @bot.message_handler(func=lambda message: True)
 def weather(message):
-    global UNITS
-    # greetings = ['привет', 'hello', 'hi', 'hey']
-    # if any([g in str.lower(message.text) for g in greetings]):
+    global units
     if str.lower(message.text).strip() in ['привет', 'hello', 'hi', 'hey']:
-        bot.reply_to(message, f'Hi, {message.from_user.first_name}')
+        bot.reply_to(message, f'Hi, {message.from_user.first_name}, type /start to begin')
     elif str.lower(message.text) == 'metric':
-        UNITS = 'metric'
-        bot.send_message(message.chat.id, f'Done. Updated units to {UNITS}.')
+        units = 'metric'
+        bot.send_message(message.chat.id, f'Done. Updated units to {units}.')
     elif str.lower(message.text) == 'imperial':
-        UNITS = 'imperial'
-        bot.send_message(message.chat.id, f'Done. Updated units to {UNITS}.')
+        units = 'imperial'
+        bot.send_message(message.chat.id, f'Done. Updated units to {units}.')
     else:
         bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
-
-
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
+    # TODO
 
 
 bot.polling()
