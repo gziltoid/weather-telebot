24ce75f7ecdfc5fc74e951d4db9aeaa3a644c78f
gziltoid <g.ziltoid@gmail.com>
2020-09-05 17:40:54 -0700

    add new commands and message handlers

--- weather_bot.py
+++ weather_bot.py
@@ -13,29 +13,53 @@ GET_NOTIFICATIONS = False
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(
         message.chat.id,
         '''Hey! I'm the Weather Cat.
 I can show you the weather forecast up for 5 days.
+/help - view available commands
+
+To start, send a location pin or enter your area or city:
+'''
+    )
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(
+        message.chat.id,
+        '''What can I do?
 Just send me one of these commands:
 /current - get the current weather for a location or city
 /weatherfull - get a 5-day forecast for a location or city
-/changelocation - set or change your location
-/lang - select a language
-/units - change your unit preferences
-/notifications - enable notifications
-'''
-)
+/settings - change your preferences
+''')
+
 
 @bot.message_handler(commands=['current'])
 def get_current_weather(message):
-    bot.send_message(message.chat.id, 'Current weather')
+    bot.send_message(message.chat.id, 'Current weather:')
+
 
 @bot.message_handler(commands=['weatherfull'])
 def get_forecast(message):
-    bot.send_message(message.chat.id, '5-day forecast')
+    bot.send_message(message.chat.id, '5-day forecast:')
+
+
+@bot.message_handler(commands=['settings'])
+def show_settings(message):
+    bot.send_message(
+        message.chat.id,
+        '''Settings:
+/changelocation - set or change your location
+/lang - select a language
+/units - change your unit preferences
+/notifications - enable notifications'''
+    )
+
 
 @bot.message_handler(commands=['changelocation'])
 def change_location(message):
@@ -43,9 +67,11 @@ def change_location(message):
         message.chat.id,
         f'Current location: {LOCATION}.\nSend location pin or enter your area or city:')
 
+
 @bot.message_handler(commands=['lang'])
 def change_language(message):
     bot.send_message(message.chat.id, f'Current language: {LANGUAGE}.\nSelect a language:')
+
 
 @bot.message_handler(commands=['units'])
 def change_units(message):
@@ -53,16 +79,22 @@ def change_units(message):
         message.chat.id,
         f'Current units: {UNITS}.\nIf you want to change it, please type metric or imperial:')
 
+
 @bot.message_handler(commands=['notifications'])
 def set_notifications(message):
     bot.send_message(
         message.chat.id,
         f'Enabled: {"Yes" if GET_NOTIFICATIONS else "No"}.\nDo you want to get daily notifications? Type Yes or No.')
 
+
 @bot.message_handler(func=lambda message: True)
 def weather(message):
     global UNITS
-    if str.lower(message.text) == 'metric':
+    # greetings = ['привет', 'hello', 'hi', 'hey']
+    # if any([g in str.lower(message.text) for g in greetings]):
+    if str.lower(message.text).strip() in ['привет', 'hello', 'hi', 'hey']:
+        bot.reply_to(message, f'Hi, {message.from_user.first_name}')
+    elif str.lower(message.text) == 'metric':
         UNITS = 'metric'
         bot.send_message(message.chat.id, f'Done. Updated units to {UNITS}.')
     elif str.lower(message.text) == 'imperial':
@@ -70,6 +102,7 @@ def weather(message):
         bot.send_message(message.chat.id, f'Done. Updated units to {UNITS}.')
     else:
         bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
+
 
 # @bot.message_handler(func=lambda message: True)
 # def echo_all(message):
