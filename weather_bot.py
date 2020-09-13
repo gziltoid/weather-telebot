e142400595818cc9be63751cc9b3668d011161ae
gziltoid <g.ziltoid@gmail.com>
2020-09-12 19:23:01 -0700

    improve FSM

--- weather_bot.py
+++ weather_bot.py
@@ -12,10 +12,6 @@ load_dotenv(find_dotenv())
 TOKEN = os.getenv('BOT_TOKEN')
 OWM_API_KEY = os.getenv('OWM_API_KEY')
 
-# Constants
-API_URL = 'https://api.openweathermap.org/data/2.5/forecast'
-LOCATION_DEFAULT = 'Moscow'
-
 
 # Enums
 class Units(Enum):
@@ -38,11 +34,17 @@ class State(Enum):
     SETTING_UNITS = 6
 
 
+# FIXME
 DEGREE_SIGNS = {Units.METRIC: '℃', Units.IMPERIAL: '℉'}
+
+# Constants
+API_URL = 'https://api.openweathermap.org/data/2.5/forecast'
+LOCATION_DEFAULT = 'Moscow'
+LANGUAGE_DEFAULT = Language.ENGLISH
 
 # Globals
 units = Units.METRIC
-language = Language.ENGLISH
+language = LANGUAGE_DEFAULT  # FIXME
 location = LOCATION_DEFAULT
 # are_notifications_enabled = False
 
@@ -52,7 +54,8 @@ states = {}
 
 querystring = {
     'q': location,
-    'units': units,
+    'lang': language.value,
+    'units': units.value,
     'appid': OWM_API_KEY,
 }
 
@@ -60,6 +63,7 @@ querystring = {
 # @bot.message_handler(commands=['start', 'help'])
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    # print(units, units.value, Units.METRIC, Units.METRIC.value)
     user_id = message.from_user.id
     bot.send_message(
         user_id,
@@ -74,21 +78,44 @@ Just send me one of these commands:
 To start, send a location pin or enter your city:
 ''')
     states[user_id] = State.WELCOME
+    # TODO
+    # states[user_id] = {
+    #     'state': State.WELCOME,
+    #     'settings': 'A',
+    # }
 
 
+# @bot.message_handler(func=lambda message: states[message.from_user.id]['state'] == State.WELCOME)
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, State.MAIN) == State.WELCOME)
 def welcome_handler(message):
     message_text = message.text.strip().lower()
     if message_text in ['привет', 'hello', 'hi', 'hey']:
         bot.reply_to(message, f'Hi, {message.from_user.first_name}.\nTo start, send a location pin or enter your city:')
-        # states[message.from_user.id] = State.WELCOME
+    elif message_text[0] != '/':
+        # TODO check city, location pin
+        global location
+        bot.send_chat_action(message.from_user.id, 'typing')
+        print(message.text)
+        if check_if_location_exists(message.text):
+            location = message.text.strip()
+            bot.send_message(message.from_user.id, f'Done. Current city: {location}')
+            show_commands(message)
+            states[message.from_user.id] = State.MAIN
+            # TODO switch_to_state(print_message, state)
+        else:
+            bot.send_message(message.from_user.id, 'Location not found.')
+            bot.send_message(message.from_user.id, 'To start, send a location pin or enter your city:')
     else:
-        # TODO check city
-        bot.send_message(message.from_user.id, f'Current city: {location}')
-        show_commands(message)
-        states[message.from_user.id] = State.MAIN
-        # TODO switch_to_state(print_message, state)
+        bot.send_message(message.from_user.id, 'To start, send a location pin or enter your city:')
 
+def check_if_location_exists(loc):
+    try:
+        qs = {'q': loc, 'appid': OWM_API_KEY}
+        response = requests.get(API_URL, params=qs)
+    except requests.exceptions.RequestException as e:
+        sys.stderr.write(f"Exception: {e}" + os.linesep)
+    else:
+        return response.status_code == 200
 
 def show_commands(message):
     bot.send_message(message.from_user.id, '''/current - get the current weather for a location
@@ -104,10 +131,13 @@ def main_handler(message):
     #     bot.reply_to(message, f'Hi, {message.from_user.first_name}')
     if message_text == '/current':
         get_current_weather(message)
+        show_commands(message)
     elif message_text == '/tomorrow':
         get_tomorrow_weather(message)
+        show_commands(message)
     elif message_text == '/forecast':
         get_forecast(message)
+        show_commands(message)
     elif message_text == '/settings':
         show_settings(message)
         states[message.from_user.id] = State.SETTINGS
@@ -118,17 +148,35 @@ def main_handler(message):
 def get_current_weather(message):
     bot.send_chat_action(message.from_user.id, 'typing')
     try:
-        response = requests.get(API_URL, params=querystring).json()
-        city = response['city']['name']
-        temp = int(round(response['list'][0]['main']['temp']))
-        desc = response['list'][0]['weather'][0]['description']
-        pprint(response)
-        print(f"Current weather in {city}: {temp}, {desc}")
-        bot.send_message(message.from_user.id,
-                         f"Current weather in {location}: {temp}{DEGREE_SIGNS[units]}, {desc}")
-    except Exception as e:
+        # FIXME
+        global querystring
+        querystring = {
+            'q': location,
+            'lang': language.value,
+            'units': units.value,
+            'appid': OWM_API_KEY,
+        }
+        print(querystring)
+        response = requests.get(API_URL, params=querystring)
+    except requests.exceptions.RequestException as e:
         sys.stderr.write(f"Exception: {e}" + os.linesep)
         bot.send_message(message.from_user.id, 'oops')
+    else:
+        if response.status_code == 200:
+            response = response.json()
+            city = response['city']['name']
+            country = response['city']['country']
+            temp = int(round(response['list'][0]['main']['temp']))
+            desc = response['list'][0]['weather'][0]['description']
+            # pprint(response)
+            # print(f"Current weather in {city}, {country}: {temp}, {desc}")
+            bot.send_message(message.from_user.id,
+                             f"Current weather in {city}, {country}: {temp}{DEGREE_SIGNS[units]}, {desc}")
+        else:
+            bot.send_message(message.from_user.id, response.json()['message'])
+            show_location(message)
+            # FIXME state
+            states[message.from_user.id] = State.WELCOME
 
 
 def get_tomorrow_weather(message):
@@ -146,10 +194,10 @@ def get_forecast(message):
 def show_settings(message):
     bot.send_message(
         message.from_user.id,
-        '''Settings:
-/location - change your location
-/language - select a language
-/units - change your unit preferences
+        f'''Settings:
+/location - change your location. (Current location: {location})
+/language - select a forecast language. (Current forecast language: {language.value})
+/units - change your unit preferences. (Current units: {units.value})
 /back - back to Weather''')
 
 
@@ -175,37 +223,51 @@ def settings_handler(message):
 def show_location(message):
     bot.send_message(
         message.from_user.id,
-        f'Current location: {location}.\nSend a location pin or enter your city:')
+        f'Current location: {location}.\n/back - back to Settings\nSend a location pin or enter your city:')
 
 
 def show_language(message):
-    bot.send_message(message.from_user.id, f'Current language: {language.value}.\nSelect a language: en | ru')
+    bot.send_message(message.from_user.id, f'Current forecast language: {language.value}.\n/back - back to Settings\nSelect a '
+                                           f'forecast language: en | ru')
 
 
 def show_units(message):
     bot.send_message(
         message.from_user.id,
-        f'Current units: {units.value}.\nChoose metric or imperial:')
+        f'Current units: {units.value}.\n/back - back to Settings\nChoose metric or imperial:')
 
 
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, State.MAIN) == State.SETTING_LOCATION)
 def setting_location_handler(message):
-    # TODO check city
-    bot.send_message(message.from_user.id, f'Updated. Current city: {message.text}')
-    states[message.from_user.id] = State.SETTINGS
-
+    message_text = message.text.strip()
+    if message_text != '/back':
+        # TODO check city, location pin
+        if check_if_location_exists(message_text):
+            global location
+            location = message_text
+            bot.send_message(message.from_user.id, f'Updated. Current city: {location}')
+            show_settings(message)
+            states[message.from_user.id] = State.SETTINGS
+        else:
+            bot.send_message(message.from_user.id, 'Location not found.')
+            bot.send_message(message.from_user.id, 'To start, send a location pin or enter your city:')
+    else:
+        show_settings(message)
+        states[message.from_user.id] = State.SETTINGS
 
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, State.MAIN) == State.SETTING_LANGUAGE)
 def setting_language_handler(message):
-    global language
     message_text = message.text.strip().lower()
-    if message_text == 'en':
-        language = Language.ENGLISH
-        bot.send_message(message.from_user.id, f'Updated. Current language: {language.value}.')
-        states[message.from_user.id] = State.SETTINGS
-    elif message_text == 'ru':
-        language = Language.RUSSIAN
-        bot.send_message(message.from_user.id, f'Updated. Current language: {language.value}.')
+    if message_text in ['/back', 'en', 'ru']:
+        global language
+        if message_text == 'en':
+            # TODO extract method
+            language = Language.ENGLISH
+            bot.send_message(message.from_user.id, f'Updated. Current language: {language.value}.')
+        elif message_text == 'ru':
+            language = Language.RUSSIAN
+            bot.send_message(message.from_user.id, f'Updated. Current language: {language.value}.')
+        show_settings(message)
         states[message.from_user.id] = State.SETTINGS
     else:
         bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
@@ -213,19 +275,20 @@ def setting_language_handler(message):
 
 @bot.message_handler(func=lambda message: states.get(message.from_user.id, State.MAIN) == State.SETTING_UNITS)
 def setting_units_handler(message):
-    global units
     message_text = message.text.strip().lower()
-    if message_text == 'metric':
-        units = Units.METRIC
-        bot.send_message(message.from_user.id, f'Updated. Current units: {units.value}.')
-        states[message.from_user.id] = State.SETTINGS
-    elif message_text == 'imperial':
-        units = Units.IMPERIAL
-        bot.send_message(message.from_user.id, f'Updated. Current units: {units.value}.')
+    if message_text in ['/back', 'imperial', 'metric']:
+        global units
+        if message_text == 'metric':
+            # TODO extract method
+            units = Units.METRIC
+            bot.send_message(message.from_user.id, f'Updated. Current units: {units.value}.')
+        elif message_text == 'imperial':
+            units = Units.IMPERIAL
+            bot.send_message(message.from_user.id, f'Updated. Current units: {units.value}.')
+        show_settings(message)
         states[message.from_user.id] = State.SETTINGS
     else:
         bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
-
 
 # @bot.message_handler(commands=['notifications'])
 # def set_notifications(message):
@@ -236,20 +299,9 @@ def setting_units_handler(message):
 
 
 # @bot.message_handler(func=lambda message: True)
-# def weather(message):
-#     global units
-#     # greetings = ['привет', 'hello', 'hi', 'hey']
-#     # if any([g in message.text.strip().lower() for g in greetings]):
-#     if message.text.strip().lower() in ['привет', 'hello', 'hi', 'hey']:
-#         bot.reply_to(message, f'Hi, {message.from_user.first_name}.')
-#     elif message.text.lower() == 'metric':
-#         units = 'metric'
-#         bot.send_message(message.from_user.id, f'Done. Updated units to {units}.')
-#     elif message.text.lower() == 'imperial':
-#         units = 'imperial'
-#         bot.send_message(message.from_user.id, f'Done. Updated units to {units}.')
-#     else:
-#         bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
+# def other(message):
+#     bot.reply_to(message, 'Sorry, I didn\'t get what you mean.')
+
 
 if __name__ == '__main__':
     if TOKEN is None:
